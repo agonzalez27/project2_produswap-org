669cb29be4af663fff445ebc97583c6fdc61a1a2
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2016-10-30 20:52:27 -0700

    added session but needs to be reworked users can now log in create new profiles and delete accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+  # before_action :set_user, only: [:show, :edit, :update, :delete]
   def index
     @users = User.all
   end
 
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
@@ -17,8 +22,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+    redirect_to @user
+  else
+    render :edit
+    end
+  end
+
+  def destroy 
+    User.find(params[:id]).destroy
+    redirect_to new_user_path
+  end
+
 private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :zip_code, :rules, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
