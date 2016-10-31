12ac4b9188e78776933d697df887bdba56c2cee9
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2016-10-31 10:16:13 -0700

    working on creating posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:content, :summary))
+    if @post.save
+      redirect_to user_posts_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(params.require(:post).permit(:content, :summary))
+      redirect_to user_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_posts_path
+  end
+end
